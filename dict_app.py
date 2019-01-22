892ade014bbc3db60ff43dcef32ff6b0fe25351d
Rusty Blewitt <rusty.blewitt@griffithuni.edu.au>
2019-01-22 08:42:31 +0100

    Added false input handling

--- dict_app.py
+++ dict_app.py
@@ -1,13 +1,27 @@
 import json
+import difflib
+from difflib import SequenceMatcher, get_close_matches
 
-data = json.load(open("data.json"))
-
+### Returns a key value pair of input word / actual word and definition ###
 def translate(word):
     if word in data:
-        return data[word]
+        return {word: data[word]}
+
     else:
-        return "Word not found, please try again."
+        # get_close_matches with n = 1 will return list of size 1, containing closest matches of min 0.6 similarity ratio
+        match = get_close_matches(word, data.keys(), n = 1, cutoff = 0.6)
 
-word = input("Enter word: ")
+        if len(match) == 0:
+            return {word: "Word not found"}
+        else:
+            # {Best match found: Dict lookup for best match found}
+            return {match[0]: data[match[0]]}
 
-print(translate(word))
+
+data = json.load(open("data.json"))
+word = input("Enter word: ").lower()
+
+definitions = translate(word)
+print(definitions)
+
+# print(data)
